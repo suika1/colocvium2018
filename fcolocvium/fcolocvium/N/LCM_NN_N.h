ec59643fb581fbd8a7b0a068853d8803b81e4ac3
Ivanov Daniil <h0n3ybadg3r31337@gmail.com>
2018-03-29 19:05:52 +0300

    The LCM_NN_N module is ready

---
+++ fcolocvium/fcolocvium/N/LCM_NN_N.h
@@ -0,0 +1,19 @@
+#ifndef _LCM_NN_N_H_
+#define _LCM_NN_N_H_
+#include "longNat.h"
+
+/*
+* nokNumbN
+* 
+* Функция считает нок 2-ух чисел
+* 
+* Параметры:
+* 1) fNumb - Первое число
+* 2) sNumb - Второе число
+* 
+* Функция возращает тип - NUMBN
+* 
+*/
+NUMBN nokNumbN(NUMBN fNumb, NUMBN sNumb);
+
+#endif
