c1220c8857cd500eb2d34124279bf7dc453f686a
Ivan_Kr <vanyoktank1@mail.ru>
2018-03-30 21:21:28 +0300

    Added _NZER_N_B

---
+++ fcolocvium/fcolocvium/N/_NZER_N_B.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "longNat.h"
+#include "_NZER_N_B.h"
+
+/*
+Функция для проверки числа на ноль,
+1 - не явл. нулем, 0 - явл. нулем, -1 - ошибка
+
+Параметры:
+1) a - Первое число
+
+return: int
+*/
+
+int isZero(LNGNT x)
+{
+	int ans = 0;
+	if (x.A)
+	{
+		for (int i = 0; i < x.n; i++)
+			if (x.A[i] != 0)
+				ans = 1;
+	}
+	else
+	{
+		printf("\nОшибка, числа не существует!\n");
+		ans = -1;
+	}
+
+	return ans;
+}
---
+++ fcolocvium/fcolocvium/N/_NZER_N_B.h
@@ -0,0 +1,16 @@
+#ifndef _NZER_N_B_H_
+#define _NZER_N_B_H_
+
+#include "numbN.h"
+
+/*
+Функция для проверки числа на ноль,
+1 - не явл. нулем, 0 - явл. нулем, -1 - ошибка
+
+Параметры:
+1) a - Первое число
+
+return: int
+*/
+int isZero(LNGNT a);
+#endif
